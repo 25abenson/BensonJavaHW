c4ba291b438f386b7b52a5f0cc72d2a43836053c
25abenson <147645415+25abenson@users.noreply.github.com>
2024-10-27 20:28:09 -0400

    added comments to part D

--- Unit04/Lesson09/PartD.java
+++ Unit04/Lesson09/PartD.java
@@ -18,11 +18,14 @@ class DieRoll{
     private int dieface = 0;
 
     public void rollDie(){
+        //this randomizes the dice roll to an integer between 1 and 6
         dieface = (int) ((Math.random() * 6) + 1);
     }
     public void setDie(){
+        //this allows the user to set ethe die
         System.out.print("What would you like to set the die to?:");
         dieface = input.nextInt();
+        //if they go outside of the 1-6 range they will see this error message
         if (dieface > 6 ){
             System.err.println("You can only set the die to numbers between 1-6, please try again.");
             System.out.print("What would you like to set the die to?:");
@@ -35,6 +38,7 @@ class DieRoll{
 
     }
     public void displayDie(){
+        //this allows the user to view the die, it is in a format that looks like a virtual dice
         if (dieface == 1){
             System.out.println("-----------");
             System.out.println("|         |");
@@ -78,31 +82,39 @@ class DieRoll{
 
 public class PartD {
     public static void main(String[] args) {
+        //creates an instance of the DieRoll class
         DieRoll die = new DieRoll();
+        //creates scanner instance
         Scanner in = new Scanner (System.in);
+        //displays the message to user and sets the input as a string
         System.out.print("Enter 'r' to roll, 'd' to display, 's' to set, or 'q' to quit: ");
         String userInput = in.nextLine();
         
         while (userInput.toUpperCase().equals("Q") == false) {
+            //as long as the user does not type Q this loop will continue
 
         if (userInput.toUpperCase().equals("R")){
+            //rolls the die if R is typed and then allows user to input something else
             die.rollDie();
             System.out.print("Enter 'r' to roll, 'd' to display, 's' to set, or 'q' to quit: ");
             userInput = in.nextLine();
         } else if (userInput.toUpperCase().equals("S")){
+            //sets the die if S is typed and then allows user to input something else
             die.setDie();
             System.out.print("Enter 'r' to roll, 'd' to display, 's' to set, or 'q' to quit: ");
             userInput = in.nextLine();
         } else if (userInput.toUpperCase().equals("D")){
+            //displays the die if D is typed and then allows user to input something else
             die.displayDie();
             System.out.print("Enter 'r' to roll, 'd' to display, 's' to set, or 'q' to quit: ");
             userInput = in.nextLine();
         } else {
+            //if not one of the four options is entered, the user gets an error message and is asked to reenter something else
             System.err.println("This is not an option for interacting with the die. Please try again.");
             System.out.print("Enter 'r' to roll, 'd' to display, 's' to set, or 'q' to quit: ");
             userInput = in.nextLine();
 }
-}
+}   //if Q is typed the program ends with this message
     System.out.println("You have thrown the die off the table and lost it. Goodbye!");
     }
 }
