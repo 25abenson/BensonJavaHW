aa9490cea8e631d748bbf1843b280c6437e62157
25abenson <147645415+25abenson@users.noreply.github.com>
2025-02-02 17:39:27 -0500

    completed Part A of Lesson 26 HW

---
+++ Unit08/Lesson26/PartA.java
@@ -0,0 +1,19 @@
+//Alex Benson
+//Lesson 26 Part A HW
+// 2/2/25
+
+public class PartA {
+    public static void main(String[] args) {
+
+        // pi is an example of a static constant of the math class
+        final double PI = Math.PI;
+        // print out pi value
+        System.out.println("Pi = " + PI);
+
+        // the round method is a static method of the Math class
+        double piRounded = Math.round(PI);
+        // print out rounded pi value
+        System.out.println("Pi (rounded) = " + piRounded);
+
+    }
+}
