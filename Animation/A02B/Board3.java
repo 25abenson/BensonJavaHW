a821c9faf4a3fa23b0983161d44d0c4fabe274ba
25abenson <147645415+25abenson@users.noreply.github.com>
2025-03-06 09:11:34 -0500

    completed HWA02B

--- Animation/A02B/Board3.java
+++ Animation/A02B/Board3.java
@@ -20,16 +20,21 @@ import javax.swing.JPanel;
 
 public class Board3 extends JPanel {
 
-    private final int B_WIDTH = 350;
-    private final int B_HEIGHT = 350;
+    private final int B_WIDTH = 720;
+    private final int B_HEIGHT = 720;
     private int x = 0;
     private int y = 0;
     private final int DIAMETER = 20;
+    private final int DEGREE_ROTATION = 5;
 
     private Timer timer;
     private final int INITIAL_DELAY = 100;
     private final int PERIOD_INTERVAL = 25;
-    private int xSpeed = 2;
+    private int degrees = 0;
+    private int xSpeed = 1;
+    private int ySpeed = 1;
+
+    private BufferedImage img;
 
     private class ScheduledUpdate extends TimerTask {
         /*
@@ -38,9 +43,19 @@ public class Board3 extends JPanel {
          */
         public void run() {
             x += xSpeed;
+            y += ySpeed;
+
+            // increases degree by 5 each time
+            degrees += DEGREE_ROTATION;
+
+            // resets to be top corner
             if (x > B_WIDTH) {
                 x = 0;
+            } // resets to be top corner
+            if (y > B_HEIGHT) {
+                y = 0;
             }
+            // regenerates image
             repaint();
         }
     }
@@ -53,11 +68,19 @@ public class Board3 extends JPanel {
         // to be on the left side of the content area
         // and in the middle of the content area.
         x = 0;
-        y = B_HEIGHT / 2;
+        y = 0;
 
         timer = new Timer();
         timer.scheduleAtFixedRate(new ScheduledUpdate(),
                 INITIAL_DELAY, PERIOD_INTERVAL);
+
+        // attempt to load the image.
+        try {
+            File imageFile = new File("media/Andy.png");
+            img = ImageIO.read(imageFile);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
 
     }
 
@@ -68,13 +91,23 @@ public class Board3 extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
 
         AffineTransform affineTransform = new AffineTransform();
-        affineTransform.translate(x - DIAMETER / 2, y - DIAMETER / 2);
 
-        // draw a filled elippse using transformed location
-        Ellipse2D ellipse = new Ellipse2D.Double(0, 0, DIAMETER, DIAMETER);
-        Shape transformedShape = affineTransform.createTransformedShape(ellipse);
-        g2d.setColor(Color.MAGENTA);
-        g2d.fill(transformedShape);
+        // gets the image width
+        int SIDE_LEN = img.getWidth();
+
+        if (img != null) {
+
+            // moves diagonally
+            affineTransform.translate(x, y);
+            // rotates by degrees with center at the middle of image
+            affineTransform.rotate(Math.toRadians(degrees), SIDE_LEN / 2, SIDE_LEN / 2);
+
+            // draws the image
+            g2d.drawImage(img, affineTransform, null);
+        } else {
+            g2d.setColor(Color.BLUE);
+            g2d.drawString("Unable to load image!", 25, 25);
+        }
 
     }
 
